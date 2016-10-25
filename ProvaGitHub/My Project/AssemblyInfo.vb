aa3e8090988b68cd1197fb5b4a6f419ad17d7af3
fabiocardarola <fabio.cardarola@gmail.com>
2016-10-25 11:36:49 +0200

    Commit iniziale

---
+++ ProvaGitHub/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Le informazioni generali relative a un assembly sono controllate dal seguente 
+' insieme di attributi. Per modificare le informazioni associate a un assembly
+' occorre quindi modificare i valori di questi attributi.
+
+' Rivedere i valori degli attributi dell'assembly
+
+<Assembly: AssemblyTitle("ProvaGitHub")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("ProvaGitHub")> 
+<Assembly: AssemblyCopyright("Copyright ©  2016")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'Se il progetto viene esposto a COM, il GUID che segue verrà utilizzato per creare l'ID della libreria dei tipi
+<Assembly: Guid("373a654c-8447-4fed-acb1-dc524d764791")> 
+
+' Le informazioni sulla versione di un assembly sono costituite dai seguenti quattro valori:
+'
+'      Numero di versione principale
+'      Numero di versione secondario 
+'      Numero build
+'      Revisione
+'
+' È possibile specificare tutti i valori oppure impostare valori predefiniti per i numeri relativi alla revisione e alla build 
+' utilizzando l'asterisco (*) come descritto di seguito:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
